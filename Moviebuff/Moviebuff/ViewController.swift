d41f4473d00aee1a6213a93294fa5e0a2efd4554
Dushyant Singh <dushyantsingh1097@gmail.com>
2019-07-14 17:08:56 +0800

    Created project and added pod files

---
+++ Moviebuff/Moviebuff/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moviebuff
+//
+//  Created by Dushyant Singh on 14/7/19.
+//  Copyright © 2019 Dushyant Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Moviebuff/Pods/Target Support Files/Pods-Moviebuff/Pods-Moviebuff-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Moviebuff : NSObject
+@end
+@implementation PodsDummy_Pods_Moviebuff
+@end
